ab325f758111960e9844d934a404934c6e456878
Tom Lorimer <tom@purplehippo.io>
2017-10-27 11:01:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 $(window).on("load", function() {
-	$('.body-content-view').prepend('<div class="test-div"><h3>Test Div</h3><div class="slider"></div></div>');
+	var uipanel;
+	uipanel = '<div class="test-div"><h3>Test Div</h3><div class="ui-wrapper"><div class="slider"></div></div></div>';
+
+
+
+	$('.body-content-view').prepend(uipanel);
+
 
 	$( ".slider" ).slider({
   		animate: "fast"
