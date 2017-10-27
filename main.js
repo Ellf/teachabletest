7a1e778b026918b00bd6d3d46fe8e507cdd0a428
Tom Lorimer <tom@purplehippo.io>
2017-10-27 10:52:17 +0100

    trying out jqueryui

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $(window).on("load", function() {
-	$('.body-content-view').prepend('<div class="test-div"><h3>Test Div</h3></div>');
+	$('.body-content-view').prepend('<div class="test-div"><h3>Test Div</h3><div class="slider"></div></div>');
+
+	$( ".slider" ).slider({
+  		animate: "fast"
+	});
 });
