d1ba173dd1793ca179ecc93aff63b047fc1d8888
Tom Lorimer <tom@purplehippo.io>
2017-10-27 09:40:36 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 $(window).on("load", function() {
-	$('body').append('<div style="width:200px; height: 400px; background: red; position: relative; float:right; top: 200px;">Test Div</div>');
+	$('.body-content-view').prepend('<div style="width:200px; height: 400px; background: red; position: relative; float:right; top: 200px;">Test Div</div>');
 });
