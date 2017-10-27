333804be910748aece1078aa9dcade9c99644d60
Tom Lorimer <tom@purplehippo.io>
2017-10-27 21:23:53 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(window).on("load", function() {
 	var sectionTitleFontSize = $("h2.section-title").css('font-size');
 	console.log(sectionTitleFontSize); // 22px
 
-	uipanel = '<div class="test-div"><h3>Test Div v0.0.1</h3><div class="ui-wrapper"><div id="slider1"></div></div></div>';
+	uipanel = '<div class="test-div"><h3>Teachable Stylist v0.0.2</h3><div class="ui-wrapper"><div id="slider1"></div></div></div>';
 
 
 	$('.body-content-view').prepend(uipanel);
