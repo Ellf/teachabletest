c4e4a57621a911ab36235d2c659f053b6b2b8ebe
Tom Lorimer <tom@purplehippo.io>
2017-10-27 15:17:21 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(window).on("load", function() {
 
 	var uipanel;
 	var sectionTitleFontSize = $("h2.section-title").css('font-size');
+	console.log(sectionTitleFontSize);
 
 	uipanel = '<div class="test-div"><h3>Test Div</h3><div class="ui-wrapper"><div class="slider"></div></div></div>';
 
@@ -11,6 +12,9 @@ $(window).on("load", function() {
 
 	$( ".slider" ).slider({
   		animate: "fast",
+  		range: true,
+  		max: 60,
+  		min: 8,
   		value: sectionTitleFontSize
 	});
 
